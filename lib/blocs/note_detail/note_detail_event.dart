2802b493d5987960768fef640bd75cad05c7612e
Nikita Kozlov <nikita@mac-mini.local>
2020-09-25 20:13:23 +0300

    add note_detail_event

--- lib/blocs/note_detail/note_detail_event.dart
+++ lib/blocs/note_detail/note_detail_event.dart
@@ -6,3 +6,38 @@ abstract class NoteDetailEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class NoteLoaded extends NoteDetailEvent {
+  final Note note;
+  const NoteLoaded({@required this.note});
+  List<Object> get props => [note];
+
+  @override
+  String toString() => 'NoteLoaded { note: $note }';
+}
+
+class NoteContentUpdated extends NoteDetailEvent {
+  final String content;
+
+  NoteContentUpdated({@required this.content});
+  List<Object> get props => [content];
+
+  @override
+  String toString() => 'NoteContentUpdated { content: $content }';
+}
+
+class NoteColorUpdated extends NoteDetailEvent {
+  final Color color;
+
+  NoteColorUpdated({@required this.color});
+  List<Object> get props => [color];
+
+  @override
+  String toString() => 'NoteContentUpdated { color: $color }';
+}
+
+class NoteAdded extends NoteDetailEvent {}
+
+class NoteSaved extends NoteDetailEvent {}
+
+class NoteDeleted extends NoteDetailEvent {}
